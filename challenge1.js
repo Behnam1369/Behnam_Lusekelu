473b061083bac2a3470ce0dcf2417367063a26d9
Behnam1369 <Behnam.Aghaali@yahoo.com>
2021-12-02 12:33:17 +0330

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,14 @@
-function NumberOfFruitsDroppedInHouse(){
-
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  var countApple = 0;
+  var countOrange = 0;
+  apples.map(function (el) {
+    if (el + a >= s && el + a <= t) countApple++;
+  });
+  oranges.map(function (el) {
+    if (el + b >= s && el + b <= t) countOrange++;
+  });
+  console.log(countApple);
+  console.log(countOrange);
 }
+
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
